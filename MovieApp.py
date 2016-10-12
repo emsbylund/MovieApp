179b74f3c0505e8b483f815cf6b3ab21c17998f9
emsbylund <emma.bylund@live.se>
2016-10-12 10:24:18 +0200

    -

---
+++ MovieApp.py
@@ -0,0 +1,11 @@
+# *-* coding:utf-8 *-*
+
+import bottle
+from bottle import route, get, post, run, template, error, static_file, request, redirect, abort, response, app
+
+
+
+
+
+
+run(host='localhost', port=8000, debug=True, reloader=True)
