832e6ea52703d4c049073f07e428d45399dc3331
sofialundkvist <sofialundkvist@outlook.com>
2016-10-12 11:16:37 +0200

    Lagt till Call_Api-funktion

--- MovieApp.py
+++ MovieApp.py
@@ -5,8 +5,24 @@ from bottle import route, get, post, run, template, error, static_file, request,
 import requests, json
 
 def Call_Api(url):
-    base_url="http://omdbapi.com/?"
+    base_url = "http://omdbapi.com/?"
 
+    response = requests.get(base_url + url)
 
+    try:
+        if response.status_code() == 200:
+            return response.json()
+        else:
+            return "Något gick fel!"
+
+def Search_Movie():
+    title = "The Notebook"
+    title.replace(" ", "+")
+    url = "t=" + title + "&y=&plot=short&r=json"
+
+    json_movie_data = Call_Api(url)
+    print json_movie_data
+
+Search_Movie()
 
 run(host='localhost', port=8080, debug=True, reloader=True)
