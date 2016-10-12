6333e20c6caba46766e487020a0b7738f4d7c5c3
emsbylund <emma.bylund@live.se>
2016-10-12 10:39:37 +0200

    -

--- MovieApp.py
+++ MovieApp.py
@@ -2,10 +2,11 @@
 
 import bottle
 from bottle import route, get, post, run, template, error, static_file, request, redirect, abort, response, app
+import requests, json
+
+def Call_Api(url):
+    base_url="http://omdbapi.com/?"
 
 
 
-
-
-
-run(host='localhost', port=8000, debug=True, reloader=True)
+run(host='localhost', port=8080, debug=True, reloader=True)
